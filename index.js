660353fb97a427d2e6eeabdba90063e2a72d3632
khs-kks <95934427+khs-kks@users.noreply.github.com>
2023-06-16 14:02:54 +0200

    3.13: Phonebook database, step1
    Change the fetching of all phonebook entries so that the data is fetched from the database.

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors')
+require('dotenv').config()
 
+const Person = require('./models/person')
 
 const app = express();
-// app.use(cors())
+app.use(cors())
 
 morgan.token('postData', (req) => {
     return JSON.stringify(req.body);
@@ -15,35 +17,18 @@ app.use(morgan(':method :url :status :res[content-length] - :response-time ms :p
 app.use(express.static('build'));
 app.use(express.json());
 
-const generateId = () => {
-    return Math.floor(Math.random() * 1000000);
-}
+// const generateId = () => {
+//     return Math.floor(Math.random() * 1000000);
+// }
 
 let persons = [
-    {
-        "id": 1,
-        "name": "Arto Hellas",
-        "number": "040-123456"
-    },
-    {
-        "id": 2,
-        "name": "Ada Lovelace",
-        "number": "39-44-5323523"
-    },
-    {
-        "id": 3,
-        "name": "Dan Abramov",
-        "number": "12-43-234345"
-    },
-    {
-        "id": 4,
-        "name": "Mary Poppendieck",
-        "number": "39-23-6423122"
-    }
+
 ];
 
 app.get('/api/persons', (req, res) => {
-    res.json(persons);
+    Person.find({}).then((persons) => {
+        res.json(persons);
+    })
 })
 
 app.get("/info", (req, res) => {
@@ -102,7 +87,7 @@ const unknownEndpoint = (req, res) => {
 
 app.use(unknownEndpoint);
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
